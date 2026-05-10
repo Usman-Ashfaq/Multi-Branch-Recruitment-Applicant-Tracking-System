f65459b710b9b9241385b44bf9c43e79eabd4d4c
Usman Ashfaq <156636786+Usman-Ashfaq@users.noreply.github.com>
2026-05-10 19:47:04 +0500

    Update server.js

--- Main backend/server.js
+++ Main backend/server.js
@@ -38,6 +38,12 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
+
+// At the bottom of server.js
+module.exports = app;
